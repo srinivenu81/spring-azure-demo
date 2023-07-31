34a8edc6f40e53dac4bd9824bc104b8b5853c371
srinivasan <srinivenu81@gmail.com>
2023-07-31 14:16:22 +0530

    first commit

---
+++ src/main/java/com/demo/springazuredemo/SpringAzureDemoApplication.java
@@ -0,0 +1,15 @@
+package com.demo.springazuredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+
+public class SpringAzureDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzureDemoApplication.class, args);
+	}
+
+}
